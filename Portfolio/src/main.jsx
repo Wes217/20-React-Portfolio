30e90b7c79a403401aa0c95e7045b8ee23776292
Lendo <wesreedbusiness@gmail.com>
2023-11-09 17:28:25 -0600

    init

---
+++ Portfolio/src/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: 'about',
+        element: <AboutPage />,
+      }
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+)
