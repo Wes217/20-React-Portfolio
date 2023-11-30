612ee0a5c2ab8418ecd6a68c4f338a490ecfe61d
Lendo <wesreedbusiness@gmail.com>
2023-11-29 22:43:32 -0600

    got nav and about page to show and css

---
+++ Portfolio/src/components/Nav.jsx
@@ -0,0 +1,12 @@
+import "../components/Nav.css"
+
+
+
+export default function Nav() {
+
+    return(
+        <div className="container">
+            <h1>hello</h1>
+        </div>
+    )
+}
--- Portfolio/src/main.jsx
+++ Portfolio/src/main.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+// import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+
+import AboutPage from "./pages/AboutPage.jsx"
+import ErrorPage from './pages/ErrorPage.jsx';
 
 const router = createBrowserRouter([
   {
